845c1745a7edf528c1287b9d98f1384f9af59085
JGMelon22 <jgvasconcellos22@gmail.com>
2025-11-13 21:41:52 -0300

    feat(infrastructure): implement ApplicationDbContext
    - Create DbContext with DbSets for all entities
    - Apply Entities configurations
    - Prepare structure for migrations

---
+++ src/StudentsRegistrationSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsRegistrationSystem.Core.Alunos.Domains.Entities;
+using StudentsRegistrationSystem.Core.Cursos.Domains.Entities;
+using StudentsRegistrationSystem.Core.Matriculas.Domains.Entities;
+using StudentsRegistrationSystem.Infrastructure.Data.Configurations;
+
+namespace StudentsRegistrationSystem.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Curso> Cursos { get; set; }
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Matricula> Matriculas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new AlunoConfiguration());
+        modelBuilder.ApplyConfiguration(new CursoConfiguration());
+        modelBuilder.ApplyConfiguration(new MatriculaConfiguration());
+    }
+}
